eb80e5554f75de5f4c218f146eb9d511e8357121
mark-ianz <bustillomarkian23@gmail.com>
2024-08-12 12:17:33 +0800

    change back origin to localhost

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://fitracker-app.netlify.app",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
