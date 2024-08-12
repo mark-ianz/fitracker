046a3debebdc08e713d7da65232a5ba85dac2549
mark-ianz <bustillomarkian23@gmail.com>
2024-08-12 12:49:08 +0800

    Change build script to hide the src

--- frontend/src/components/WorkoutsFeed/Create/WokroutSessionForm.jsx
+++ frontend/src/components/WorkoutsFeed/Create/WokroutSessionForm.jsx
@@ -37,7 +37,7 @@ const WokroutSessionForm = () => {
     };
 
 
-    const response = await fetch("http://localhost:8080/api/workouts/new", {
+    const response = await fetch("https://fitracker.onrender.com/api/workouts/new", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
