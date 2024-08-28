a657627a87052bfb2ab566097482a3f91d9b2873
[mark-ianz] <bustillomarkian23@gmail.com>
2024-08-28 17:44:36 +0800

    fixed features

--- frontend/src/pages/Features.jsx
+++ frontend/src/pages/Features.jsx
@@ -3,13 +3,19 @@ import { features } from "../utils/constants";
 const Features = () => {
   return (
     <main className="px-4 sm:px-6 lg:px-8">
-      <h1 className="text-4xl font-bold text-gray-900 mb-8 text-center">
+      <h1 className="text-4xl font-bold mb-8 text-center text-red-400">
         Features
       </h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {features.map((feature) => (
-          <li className="bg-white p-6 rounded-md shadow-md" key={feature.id}>
-            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <li className="bg-white p-6 rounded-md shadow-md flex flex-col items-center gap-4" key={feature.id}>
+            <img
+              src={feature.image}
+              alt={feature.title + "image"}
+              className={`w-64 aspect-square
+              ${feature.filter ? "filter-secondary" : ""}`}
+            />
+            <h2 className="text-2xl font-semibold text-red-400 mb-4 text-center">
               {feature.title}
             </h2>
             <p className="text-gray-600">{feature.description}</p>
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,17 +1,11 @@
 import { generate_uuid } from "./helper";
 
-export const features = [
+/* export const features = [
   {
     id: generate_uuid(),
     title: "Workout Session Logging",
     description:
       "Easily log your workout sessions by entering details such as the session name, description, location, date, and tags.",
-  },
-  {
-    id: generate_uuid(),
-    title: "Exercise Input",
-    description:
-      "Input the exercises you performed during your session, including sets, reps, and weights.",
   },
   {
     id: generate_uuid(),
@@ -21,20 +15,51 @@ export const features = [
   },
   {
     id: generate_uuid(),
-    title: "Recommended Workout Splits",
-    description:
-      "Browse through a variety of recommended workout splits tailored to different fitness goals.",
-  },
-  {
-    id: generate_uuid(),
     title: "One-Click Split Logging",
     description:
       "Log a recommended workout split with a single click, automatically filling in the exercises with adjustable sets and reps.",
   },
   {
     id: generate_uuid(),
+    title: "Recommended Workout Splits",
+    description:
+      "Browse through a variety of recommended workout splits tailored to different fitness goals.",
+  },
+  {
+    id: generate_uuid(),
+    title: "Exercise Input",
+    description:
+      "Input the exercises you performed during your session, including sets, reps, and weights.",
+  },
+  {
+    id: generate_uuid(),
     title: "Custom Workout Splits",
     description:
       "Create and save your own workout splits, tailored to your unique fitness goals and preferences.",
+  },
+]; */
+
+export const features = [
+  {
+    id: generate_uuid(),
+    title: "Track Every Detail of Your Workout",
+    description:
+      "Effortlessly log every aspect of your workout sessions. Whether it’s the exercises performed, the number of sets, reps, or the weights lifted, our platform makes it simple to capture and review your progress. Stay on top of your fitness goals by tracking your improvements over time, and never miss a detail of your fitness journey.",
+    image: "/feature_1.png",
+  },
+  {
+    id: generate_uuid(),
+    title: "Secure and Personalized Experience",
+    description:
+      "Enjoy a secure and experience with our authentication system. Your data such as workout history and custom plans is protected and available only to you. Benefit from a seamless and synchronized experience as you work towards your fitness goals.",
+    image: "/feature_2.png",
+  },
+  {
+    id: generate_uuid(),
+    title: "Create and Customize Your Ideal Workout Plan",
+    description:
+      "Design workout plans that fit your unique needs and preferences. Choose from expert-recommended splits or create your own custom routines, targeting specific muscle groups and fitness goals. Our interface allows you to easily adjust and refine your workouts, giving you the flexibility to train your way",
+    image: "/feature_3.png",
+    filter: true,
   },
 ];
